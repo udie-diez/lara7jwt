9ae50e6b83e65375c353ba730d951497db58b24b
udie-diez <warewolflycans@gmail.com>
2022-10-31 21:48:16 +0700

    fixed Non-static method JWTAuth

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Exception;
 use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
-use Tymon\JWTAuth\JWTAuth;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class JwtMiddleware extends BaseMiddleware
 {
@@ -26,22 +26,23 @@ class JwtMiddleware extends BaseMiddleware
                     'status' => 'error',
                     'message' => 'Token is blacklisted'
                 ], 400);
-            } elseif ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
+            }
+            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
                 return response()->json([
                     'status' => 'error',
                     'message' => 'Token is expired'
                 ], 401);
-            } elseif ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
+            }
+            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
                 return response()->json([
                     'status' => 'error',
                     'message' => 'Token is invalid'
                 ], 403);
-            } else {
-                return response()->json([
-                    'status' => 'error',
-                    'message' => 'Authorization token not found'
-                ], 404);
             }
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Authorization token not found'
+            ], 404);
         }
 
         return $next($request);
